95bc40b759654d06171ae05786e8351a6adda2f3
Axel Pestoni <axelkpesto@gmail.com>
2022-08-04 11:04:04 -0700

    process ordered

--- index.js
+++ index.js
@@ -48,8 +48,6 @@ io.on('connection', async (socket) => {
 
     console.log('a user connected');
     console.log("UserCount: " + io.engine.clientsCount);
-    newQuestion();
-
 
     io.emit('userNumber',io.engine.clientsCount);
 
